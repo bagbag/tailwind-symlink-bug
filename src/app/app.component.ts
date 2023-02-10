4d78329d84ecc10cf5e9c2f832baa857d6a75f54
Patrick Hein <patrick-hein@outlook.com>
2023-02-10 19:48:28 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="w-40 h-40 bg-red-500"></div>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'tailwind-symlink-bug';
+}
